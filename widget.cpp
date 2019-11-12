8b3c128e734be9321eb3c7fac7a63edb613eea9c
450157081@qq.com <450157081@qq.com>
2019-11-12 18:58:50 +0800

    版本：0.01 说明：刚开始，什么都没开始

---
+++ widget.cpp
@@ -0,0 +1,126 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setFixedSize(this->width(),this->height());
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_btnResult_clicked()
+{
+    QString str=ui->lineEdit->text();
+    int posL=str.lastIndexOf("(");
+    int posR=-1;
+    QString strTemp="";
+    ui->lineEdit->setText(QString::asprintf("%d",str.length()));
+    if(posL)
+    {
+        for (int i=posL;i<str.length();i++)
+        {
+            if (str[i]==")")
+            {
+               posR=i ;
+            }
+        }
+    }
+    if(posR>posL)
+    {
+        for (int i=posL+1;i<posR;i++)
+        {
+            strTemp+=str[i];
+        }
+    }
+
+    //ui->lineEdit->setText(strTemp);
+    //ui->lineEdit->setText(QString::asprintf("%d----%d",posL,posR));
+}
+
+QString Widget::BasicCalculate(QString str)
+{
+    QList<QString> list,poslist;
+    QString strOp="+-*/";
+    QString tempStr="";
+    QString tempOp="";
+    QString strReturn="";
+    int temp =0;
+    int pos=0;
+    for (int i=0;i<str.length();i++)
+    {
+        for (int j=0;j<strOp;j++)
+        {
+            if(str[i]==strOp[j])
+            {
+               temp++;
+               tempOp+=str[i];
+               poslist.append(tempOp);
+               tempOp.clear();
+               for (int k=pos;k<i;k++)
+               {
+                    tempStr+=str[k];
+                    tempOp+=strOp[j];
+               }
+               list.append(tempStr);
+               list.append(tempOp);
+               tempOp.clear();
+               tempStr.clear();
+               pos=i+1;
+            }
+        }
+    }
+    if(temp==0)
+        return str;
+
+    for (int k=pos;k<str.length();k++)
+        tempStr+=str[k];
+    list.append(tempStr);
+
+    QList<QString> num;
+    for (int i=0;i<temp;i++)
+    {
+        switch (str[poslist[i].toUInt()].unicode())
+        {
+        case '*':
+            num.append("2");
+            break;
+        case '/':
+            num.append("2");
+            break;
+        case '+':
+            num.append("1");
+            break;
+        case '-':
+            num.append("1");
+            break;
+        default:
+            break;
+        }
+    }
+    num+="0";
+    double Sum=0;
+    for (int i=1;i<num.length()-1;i++)
+    {
+        if(num[i].toInt()>=num[i-1].toInt()&&num[i].toInt()>=num[i+1].toInt())
+        {
+            Sum=list[2*i-2].toDouble()*list[2*i].toDouble();
+
+            list.removeAt(2*i);
+            list.removeAt(2*i);
+            list.removeAt(2*i);
+            list.insert(2*i,QString::asprintf("%f",Sum));
+            for (int j=0;j<list.size();j++)
+            {
+                  strReturn+=list[j];
+            }
+            return BasicCalculate(strReturn);
+        }
+    }
+
+}
---
+++ widget.h
@@ -0,0 +1,28 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_btnResult_clicked();
+
+private:
+    Ui::Widget *ui;
+
+
+    QString BasicCalculate(QString str);
+};
+
+#endif // WIDGET_H
